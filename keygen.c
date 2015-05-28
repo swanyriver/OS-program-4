49d37f5d90184c7ebad1011bdd5aaa693d6f96f0
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-05-27 19:36:26 -0700

    functioning keygen

--- keygen.c
+++ keygen.c
@@ -9,33 +9,28 @@
 #include <time.h>
 #include <stdlib.h>
 
-int GetRandomInRange ( int min , int max );
+int main(int argc, char **argv) {
 
-int int main(int argc, char **argv) {
-    int keylength = strtol(argv[1],NULL,10);
-    for(int i=0; i<)
-}
+    //todo assert num args and int value for arg[1]
 
-/******************************************************************************
- *    purpose:produce a random number
- *
- *    entry: max >= min
- *
- *    exit: random int in range [min,max]
- *
- *    Written by Brandon Swanson during Summer Term 2014
- *     @ Oregon State University
- ******************************************************************************/
-int GetRandomInRange ( int min , int max ) {
+    /*initialize random*/
+    srand( time( NULL ) );
 
     int random;
-    /*calculate inclusive range size*/
-    int range = max - min + 1;
-    if ( range == 1 )
-        return min;
-    /*map return 0-range onto min through max*/
-    random = (rand() % range) + min;
-    return random;
+
+    int keylength = strtol(argv[1],NULL,10);
+    for(int i=0; i<keylength; ++i){
+        random = (rand() % 27);
+        if(random==26){
+            putchar(' ');
+        }
+        else{
+            putchar('A'+random);
+        }
+    }
+
+    return 0;
 }
+
 
 
