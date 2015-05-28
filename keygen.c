d7582cc1229f0eaf08203c06f69c0859e1d45289
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-05-27 18:47:13 -0700

    forgot to save file

--- keygen.c
+++ keygen.c
@@ -6,3 +6,25 @@
  */
 
 
+/******************************************************************************
+ *    purpose:produce a random number
+ *
+ *    entry: max >= min
+ *
+ *    exit: random int in range [min,max]
+ *
+ *    Written by Brandon Swanson during Summer Term 2014
+ *     @ Oregon State University
+ ******************************************************************************/
+int GetRandomInRange ( int min , int max ) {
+
+    int random;
+    /*calculate inclusive range size*/
+    int range = max - min + 1;
+    if ( range == 1 )
+        return min;
+    /*map return 0-range onto min through max*/
+    random = (rand() % range) + min;
+    return random;
+}
+
